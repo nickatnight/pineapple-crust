c87cd0da8e8d21aafbe47efc3835b0ba8ed97477
nickatnight <kay.muneee@gmail.com>
2016-10-20 23:41:16 -0700

    chore: add external volume to app...move utils from es to app

--- es/files/setup.sh
+++ es/files/setup.sh
@@ -6,10 +6,10 @@ echo '#########################'
 curl -XPOST http://127.0.0.1:9200/music -d @setup.json
 echo 'Attempting to remove old json file...'
 echo '#########################'
-rm music.json
+#rm music.json
 echo 'Writing to new json file...'
 echo '#########################'
-python utils2.py
+#python utils.py
 echo 'Pushing new json to ES...'
 echo '#########################'
 curl -s -XPOST http://127.0.0.1:9200/_bulk --data-binary @music.json
